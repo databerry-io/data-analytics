613d6c55e500e98f8b2915dce5008db37f51a95a
Eric Chen <ericchen314@gmail.com>
2023-06-29 15:15:43 -0700

    Add logging page

---
+++ pages/1_prompts.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from src.sqlite import retrieve_prompt_log
+import sqlite3
+
+st.set_page_config(layout="wide", page_title="Prompt DB", page_icon="📊")
+
+st.markdown("# Prompt Database")
+# st.sidebar.header("")
+
+conn = sqlite3.connect('prompt_log.db')
+cursor = conn.cursor()
+log_data = retrieve_prompt_log(cursor)
+
+if log_data:
+    st.header("Prompt Log")
+    st.table(log_data)
+else:
+    st.info("No prompt log entries found.")
+
+conn.close()
