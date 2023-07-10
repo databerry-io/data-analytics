78e0510f2abdaccf30ced80a7d14e07373db622b
Eric Chen <ericchen314@gmail.com>
2023-07-10 11:25:51 -0700

    Remove config.py from repo

--- config.py
+++
@@ -1,7 +0,0 @@
-from src.prompts import CustomGeneratePythonCodePrompt, CustomMultipleDataframesPrompt
-# from pandasai.prompts.multiple_dataframes import MultipleDataframesPrompt
-
-PYTHON_CODE_PROMPT = CustomGeneratePythonCodePrompt
-MULTIPLE_PYTHON_CODE_PROMPT = CustomMultipleDataframesPrompt
-
-LOGS_FILE = "logs/log.log" 
